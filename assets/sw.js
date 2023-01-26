25dc12bd41c3a596ac9e8079e0057c598ed3b999
repsacc <repsaccc@gmail.com>
2023-01-26 21:53:07 +0100

    Followed setup instructions

--- assets/sw.js
+++ assets/sw.js
@@ -2,8 +2,8 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './recipe_calculator.js',
+  './recipe_calculator_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
