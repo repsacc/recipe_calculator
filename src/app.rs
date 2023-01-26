afe4bee42aaf47c36e8b45760deacaba9243e935
Casper Renman <repsaccc@gmail.com>
2023-01-26 22:11:45 +0100

    Update app.rs

--- src/app.rs
+++ src/app.rs
@@ -95,12 +95,12 @@ impl eframe::App for TemplateApp {
         egui::CentralPanel::default().show(ctx, |ui| {
             // The central panel the region left after adding TopPanel's and SidePanel's
 
-            ui.heading("eframe template");
-            ui.hyperlink("https://github.com/emilk/eframe_template");
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/master/",
-                "Source code."
-            ));
+            // ui.heading("eframe template");
+            // ui.hyperlink("https://github.com/emilk/eframe_template");
+            // ui.add(egui::github_link_file!(
+            //     "https://github.com/emilk/eframe_template/blob/master/",
+            //     "Source code."
+            // ));
             egui::warn_if_debug_build(ui);
         });
 
